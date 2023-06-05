78df27d4b1df9453a1feaaa3915df48859ba5c50
anthony-zhou <anthony.zhou03@gmail.com>
2023-06-05 16:37:07 +0000

    Added manual translation and ChatGPT unit tests in Python

---
+++ examples/daylength_2/python/daylength.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def daylength(lat, decl):
+    """
+    Calculate the length of the day (in hours) given the latitude and the
+    declination of the sun.  This is the number of seconds between sunrise
+    and sunset. Returns NaN if input arguments are invalid. 
+
+    Parameters
+    ----------
+    lat : float or numpy array
+        Latitude of the location in radians.
+    decl : float
+        Declination of the sun in radians.
+
+    Returns
+    -------
+    float
+        Length of the day in seconds. 
+
+    """
+    # Number of seconds per radian of hour-angle
+    secs_per_radian = 13750.9871
+
+    # Epsilon for defining latitudes "near" the pole
+    lat_epsilon = 10.0 * np.finfo(float).eps
+
+    # Define an offset pole as slightly less than pi/2 to avoid problems with cos(lat) being negative
+    pole = np.pi / 2
+    offset_pole = pole - lat_epsilon
+
+    # Lat must be less than pi/2 within a small tolerance
+    # Decl must be strictly less than pi/2
+    lat = np.where(abs(lat) >= pole + lat_epsilon, np.NAN, lat)
+    decl = np.where(abs(decl) >= pole, np.NAN, decl)
+
+    
+    # Ensure that latitude isn't too close to pole, to avoid problems with cos(lat) being negative
+    my_lat = np.clip(lat, -offset_pole, offset_pole)
+    temp = - np.tan(my_lat) * np.tan(decl)
+    temp = np.clip(temp, -1, 1)
+    return 2.0 * secs_per_radian * np.arccos(temp)
+
---
+++ examples/daylength_2/python/test_daylength.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pytest
+from daylength import daylength
+
+# tolerance
+tol = 1e-3
+
+
+def test_standard_points():
+    assert np.allclose(daylength(np.array([-1.4, -1.3]), 0.1), 
+                       np.array([26125.331269192659, 33030.159082987258]), 
+                       atol=tol)
+
+def test_near_poles():
+    assert np.allclose(daylength(np.array([-1.5, 1.5]), 0.1), 
+                       np.array([0.0, 86400.0]), 
+                       atol=tol)
+
+def test_north_pole():
+    assert abs(daylength(np.pi/2.0, 0.1) - 86400.0) < tol
+    assert abs(daylength(np.pi/1.999999999999999, 0.1) - 86400.0) < tol
+
+def test_south_pole():
+    assert abs(daylength(-1.0 * np.pi/2.0, 0.1)) < tol
+    assert abs(daylength(-1.0 * np.pi/1.999999999999999, 0.1)) < tol
+
+def test_error_in_decl():
+    assert np.isnan(daylength(-1.0, -3.0))
+
+def test_error_in_lat_scalar():
+    assert np.isnan(daylength(3.0, 0.1))
+
+def test_error_in_lat_array():
+    my_result = daylength(np.array([1.0, 3.0]), 0.1)
+    assert np.isfinite(my_result[0])
+    assert np.isnan(my_result[1])
