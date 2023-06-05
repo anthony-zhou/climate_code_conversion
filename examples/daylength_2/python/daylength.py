bbf62e5f0f29bb187a3f38fa0634320ead250e34
anthony-zhou <anthony.zhou03@gmail.com>
2023-06-05 21:01:35 +0000

    Add daylength function

--- examples/daylength_2/python/daylength.py
+++ examples/daylength_2/python/daylength.py
@@ -1,10 +1,11 @@
 import numpy as np
 
+
 def daylength(lat, decl):
     """
     Calculate the length of the day (in hours) given the latitude and the
     declination of the sun.  This is the number of seconds between sunrise
-    and sunset. Returns NaN if input arguments are invalid. 
+    and sunset. Returns NaN if input arguments are invalid.
 
     Parameters
     ----------
@@ -16,7 +17,7 @@ def daylength(lat, decl):
     Returns
     -------
     float
-        Length of the day in seconds. 
+        Length of the day in seconds.
 
     """
     # Number of seconds per radian of hour-angle
@@ -34,10 +35,25 @@ def daylength(lat, decl):
     lat = np.where(abs(lat) >= pole + lat_epsilon, np.NAN, lat)
     decl = np.where(abs(decl) >= pole, np.NAN, decl)
 
-    
     # Ensure that latitude isn't too close to pole, to avoid problems with cos(lat) being negative
     my_lat = np.clip(lat, -offset_pole, offset_pole)
-    temp = - np.tan(my_lat) * np.tan(decl)
+    temp = -np.tan(my_lat) * np.tan(decl)
     temp = np.clip(temp, -1, 1)
     return 2.0 * secs_per_radian * np.arccos(temp)
 
+
+class Bounds:
+    def __init__(self, begg, endg):
+        self.begg = begg
+        self.endg = endg
+
+
+def compute_max_daylength(bounds, lat, obliquity):
+    """Compute max daylength for each grid cell"""
+    max_daylength = []
+    for g in range(bounds.begg, bounds.endg):
+        max_decl = obliquity
+        if lat[g] < 0.0:
+            max_decl = -max_decl
+        max_daylength.append(daylength(lat[g], max_decl))
+    return max_daylength
