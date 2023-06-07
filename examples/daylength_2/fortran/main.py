2604122e7625e1039fe2c00ffbeb0577cef9c535
anthony-zhou <anthony.zhou03@gmail.com>
2023-06-07 16:11:16 +0000

    Create function to generate DAGs

--- examples/daylength_2/fortran/main.py
+++ examples/daylength_2/fortran/main.py
@@ -2,35 +2,96 @@ from fparser.two.parser import ParserFactory
 from fparser.common.readfortran import FortranFileReader
 from fparser.two import Fortran2003
 
+from fparser.two import Fortran2003, parser
+from fparser.common.readfortran import FortranStringReader
+from collections import defaultdict
+import pprint
+
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
-def get_subroutines_and_functions(file_path='DaylengthMod.f90'):
-    # create the Fortran code parser
+def parse_source(file_path):
     f2003_parser = ParserFactory().create(std="f2003")
-
-    # parse the code
     reader = FortranFileReader(file_path)
     parse_tree = f2003_parser(reader)
+    return parse_tree
 
-    # function to walk the parse tree and print function, type, and subroutine names
-    def print_public_symbols(node):
-        result = []
-        if isinstance(node, (Fortran2003.Function_Subprogram, Fortran2003.Subroutine_Subprogram)):
-            result.append(node)
-        elif type(node) is not str and type(node) is not int and type(node) is not float and node is not None:
-            for child in node.children:
-                result.extend(print_public_symbols(child))
-        
-        return result
 
-    # print the names of all public functions, types, and subroutines
-    nodes = print_public_symbols(parse_tree)
+def find_calls(node, func_name=None):
+    if isinstance(
+        node, (Fortran2003.Function_Subprogram, Fortran2003.Subroutine_Subprogram)
+    ):
+        func_name = str(node.content[0].get_name())
+        print(f"Found function or subroutine {func_name}")
+        source_code = str(node)
+        yield (func_name, source_code, None)
+    if isinstance(node, Fortran2003.Call_Stmt):
+        yield (func_name, "", str(node.items[0]))
+    elif (
+        type(node) is not str
+        and type(node) is not int
+        and type(node) is not float
+        and node is not None
+        and type(node) is not tuple
+    ):
+        for child in node.children:
+            yield from find_calls(child, func_name)
 
-    print(f"Found {len(nodes)} subroutines and functions in {file_path}")
 
-    return [node.tostr() for node in nodes]
+def find_dependencies(source):
+    ast = parse_source(source)
+    dependencies = defaultdict(lambda: {"source": "", "calls": []})
+    for func_name, source_code, call in find_calls(ast):
+        if source_code != "":
+            dependencies[func_name]["source"] = source_code
+        if call is not None:
+            dependencies[func_name]["calls"].append(call)
+    return dict(dependencies)
 
-nodes = get_subroutines_and_functions()
-for node in nodes:
-    print(node)
-    print()
+
+def dependencies_to_dag(dependencies):
+    """
+    In this DAG, the nodes are functions and the edges are calls.
+    An arrow means that the function at the tail of the arrow calls the function at the head of the arrow.
+
+    Thus, a topological sort of this DAG will give us an order in which we can compile the functions.
+    """
+    dag = nx.DiGraph()
+
+    for func_name, info in dependencies.items():
+        dag.add_node(func_name, source=info["source"])
+        for call in info["calls"]:
+            dag.add_edge(call, func_name)
+
+    return dag
+
+
+def topological_sort(dependencies):
+    dag = dependencies_to_dag(dependencies)
+    return list(nx.topological_sort(dag))
+
+
+def draw_dag_and_save(dag, filename):
+    pos = nx.drawing.nx_agraph.graphviz_layout(dag, prog='dot')
+    nx.draw(dag, pos, with_labels=True, arrows=True)
+    plt.savefig(filename)
+
+
+
+# Generate a DAG from the dependencies
+
+pp = pprint.PrettyPrinter(indent=4)
+
+dependencies = find_dependencies("DaylengthMod.f90")
+sorted_functions = topological_sort(dependencies)
+
+for func_name in sorted_functions:
+    print(func_name)
+
+draw_dag_and_save(dependencies_to_dag(dependencies), "dag.png")
+
+# nodes = get_subroutines_and_functions()
+# for node in nodes:
+#     print(node)
+#     print()
