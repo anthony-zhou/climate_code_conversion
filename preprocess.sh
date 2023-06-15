e1c917fa2f6a79217c6a96e07114495894ae054b
anthony-zhou <anthony.zhou03@gmail.com>
2023-06-15 18:32:37 +0000

    Update preprocess script to take an argument

--- preprocess.sh
+++ preprocess.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Should be something like this, to preprocess a file using gfortran
 
-sed -i 's/^[ \t]*#include/#include/' DaylengthMod.f90
-gfortran -cpp -P -E DaylengthMod.f90 > preprocessed.f90 && mv preprocessed.f90 DaylengthMod.f90
+sed -i 's/^[ \t]*#include/#include/' $1
+gfortran -cpp -P -E $1 > preprocessed.f90 && mv preprocessed.f90 $1
