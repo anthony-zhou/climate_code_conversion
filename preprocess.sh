d565df34269d9bc85d35806fbdc62434c3ecdacf
anthony-zhou <anthony.zhou03@gmail.com>
2023-06-15 18:33:05 +0000

    Update preprocess script with usage

--- preprocess.sh
+++ preprocess.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Should be something like this, to preprocess a file using gfortran
+
+# Usage: ./preprocess.sh <file.f90>
 
 sed -i 's/^[ \t]*#include/#include/' $1
 gfortran -cpp -P -E $1 > preprocessed.f90 && mv preprocessed.f90 $1
