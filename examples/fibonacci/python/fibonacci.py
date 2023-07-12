95e5c2afdf8ab7601bb0f08f664dfc1cfc867b38
anthony-zhou <anthony.zhou03@gmail.com>
2023-07-12 10:40:30 +0000

    [AI] Create fibonacci.py and unit tests

--- examples/fibonacci/python/fibonacci.py
+++ examples/fibonacci/python/fibonacci.py
@@ -1,11 +1,6 @@
-
-def fibonacci(n):
-    fib = [0, 1]
-
-    for i in range(2, n+1):
-        fib.append(fib[i-1] + fib[i-2])
-
-    return fib[n]
+def sum_two_numbers(a, b):
+    sum = a + b
+    return sum
 
 
 def fibonacci(n):
